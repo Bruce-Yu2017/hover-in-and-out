6c49bbeef5d1d261db1bab2bb55224968c106672
Jiahao Yu <ilywanzi@gmail.com>
2017-08-22 08:10:08 -0700

    first submit

---
+++ hover.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("#first").hover(function() {
+        $("#first").attr("src","bunny2.png");
+    }, function() {
+        $("#first").attr("src","bunny.png")
+    })
+    
+    $("#third").hover(function() {
+        $("#third").attr("src","bunny4.png");
+    }, function() {
+        $("#third").attr("src","bunny3.png")
+    })
+    
+    $("#fifth").hover(function() {
+        $("#fifth").attr("src","bunny6.png");
+    }, function() {
+        $("#fifth").attr("src","bunny5.png")
+    })
+    
+    $("#seventh").hover(function() {
+        $("#seventh").attr("src","bunny8.png");
+    }, function() {
+        $("#seventh").attr("src","bunny7.png")
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})
